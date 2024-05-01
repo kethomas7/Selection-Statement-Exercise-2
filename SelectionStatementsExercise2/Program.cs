a853ef05c7689c094e91e7212de7c1b2cbf0d336
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-05-01 11:20:09 -0500

    Completed Exercise 2 for Selection Statements

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Ramsay High School! What is your favorite subject?");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject)
+
+            {
+                case "Math":
+                    Console.WriteLine("We will add Math to your schedule!");
+                    break;
+                case "English":
+                    Console.WriteLine("We will add English to your schedule!");
+                    break;
+                case "Spanish":
+                    Console.WriteLine("We will add Spanish to your schedule!");
+                    break;
+                case "Science":
+                    Console.WriteLine("We will add Science to your schedule!");
+                    break;
+                case "History":
+                    Console.WriteLine("We will add History to your schedule!");
+                    break;
+                default:
+                    Console.WriteLine($"We do not have {favSubject} here at Ramsay, we will add it next semester!");
+                    break;
+
+            }
         }
     }
 }
